337b6cd908cc65c593ad040fd31d992b274d6e55
MT125SKO <149218196+MT125SKO@users.noreply.github.com>
2024-02-25 13:12:46 +0900

    Add files via upload
    2СПИ_Михейченко_Л5

--- 2СПИ_Михейченко_Л5.py
+++ 2СПИ_Михейченко_Л5.py
@@ -1,74 +1,48 @@
-#Задача 1
-a = [int(input ('')) for _ in range(int(input('Введите сколько будет чисел, а затем сами числа =')))]
-print (len(set(''.join(map(str, a)))))
+#1
+def groundhog_day(a):
+    for d, (previous, current) in enumerate(zip(a, a[1:]), 1):
+        res = [ d for d, (x, y) in enumerate(zip(previous, current)) if x !=y]
+        if len(res) > 2:
+            return (d,) + tuple(res)
+    return (0, 0)
 
-#Задача 2
-a = set(input('Ведите 3 слова. Сейчас водится слово номер 1 '))
-a2 = set(input('Сейчас водится слово номер 2 '))
-a3 = set(input('Сейчас водится слово номер 3 '))
-d1 = (a&a2)
-d2 = (a2&a3)
-d3 = (a3&a)
-print (d1|d2|d3)
+data = ["Groundhog Festival in Punxsutawney.",
+        "Groundhog Festival in Punksutawney.",
+        "Groundhog Festivel in Punxsutowney."]
 
-#Задача 3
-print('Введите число')
-numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-a = input()
-print('Этих цифр небыло')
-print(set(numbers) - set(map(int, a)))
+print(groundhog_day(data))
 
-#Задача 4
-numbers = []
-number = int(input())
-while number != 0:
-    numbers.append (number)
-    number = int (input())
-count = len(numbers)
-a = [num for num in numbersа if num % count == 0]
-print(a)
-
-#Задача 5
-print ('Введите количество флагов')
-n = int(input(''))
-print ('Введите какие будут цвета')
-flag = [input('') for _ in range(n)]
-print ('Количество флагов в гирлянде')
-a = int(input(''))
-d, a2 = divmod(a,n)
-print (*(flag*d+flag[:a2]),sep='\n')
-
-#Задача 6
-years = {"Proterozoic": range(635 * 10 ** 6, 2800 * 10 ** 6), "Cenozoic": range(0, 145 * 10 ** 6),
-         "Mesozoic": range(145 * 10 ** 6, 300 * 10 ** 6), "Paleozoic": range(300 * 10 ** 6, 635 * 10 ** 6)}
-while True:
-    x = input('Укажите год: ')
-    if x == "":
-        break
-    x = int(x) * 1000
-    for i, val in years.items():
-        if x in val:
-            print(i)
-            break
-    else:
-        print("Archaea")
-
-#Задача 7
-birds = {}
-while True:
-    observation = input()
-    if observation == '':
-        print(birds)
-        break
-    bird, count = observation.split(": ")
-    if bird in birds:
-        birds[bird] += int(count)
-    else:
-        birds[bird] = int(count)
-
-#Задача 8
-a= input('')
-d = [*map( bin, map( int,a.split()))]
-s = [{'digits':len(w)-2,'units':w.count('1'),'zeros':w.count('0')-1}for w in d]
-print(s)
-
+#2
+from fractions import Fraction
+data = [[0, 2, 30, 15], [14, 3, 21, 60], [7, 16, 4, 8]]
+def gears(a1, a2, a3):
+    for lst1 in a1:
+        for el1 in lst1:
+            for lst2 in a1:
+                for el2 in lst2:
+                    if el2 != 0 and Fraction(el1, el2) == Fraction(a2, a3):
+                        return el1, el2
+print(gears(data, 30, 7))
+print(gears(data, 60, 14))
+#3
+from collections import deque
+line = "1{2 + [3}45 - 6] * (7 - 8) 9"
+line2 = "[12 / (9) + 2(5{15 * <2 - 3>}6)]"
+def staples(line2):
+    a = {']':'[', ')':'(', '}':'{'}
+    d = deque()
+    for i in line2:
+        if i in '[({':
+            d.append(i)
+        if i in '])}':
+            if len(d):
+                i = a[i]
+                if i != d.pop():
+                    return False
+            else:
+                return False
+    if len(d):
+        return False
+    return True
+print(staples(line))
+print(staples(line2))
